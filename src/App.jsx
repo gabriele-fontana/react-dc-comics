bb26f3f210ff42a06c584a280f1b9c2b4ec38336
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-03-08 23:06:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
+import AppFooter from "./components/AppFooter"
+function App() {
+  
+
+  return (
+    <>
+     <AppHeader></AppHeader>
+     <AppMain></AppMain>
+     <AppFooter></AppFooter>
+    </>
+  )
+}
+
+export default App
