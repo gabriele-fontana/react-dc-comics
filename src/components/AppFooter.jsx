20ae4471057a82357819b806b5c6739cd9847e97
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-03-09 07:10:48 +0100

    add footer markup and style

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,7 +3,46 @@ export default function AppFooter(){
 
   return (
     <footer>
-        footer
+        <div className="link-section ">
+          <div className="link-wrapper row">
+          <ul className="col-2">
+            <li><h5>DC COMICS</h5></li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+
+          </ul>
+          <ul className="col-2">
+            <li><h5>DC COMICS</h5></li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+
+
+          </ul>
+          <ul className="col-2">
+            <li><h5>DC COMICS</h5></li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+            <li>link</li>
+
+          </ul>
+
+          </div>
+          <img id='logo-bg'src="./src/assets/img/dc-logo-bg.png" alt="dclogo-bg " />
+        </div>
+
+        <div className="socials-section"></div>
     </footer>
   )
 }
